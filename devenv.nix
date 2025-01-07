7a5e136fbdf46ce7d6577115dd48c573e41d17c3
Niklaus Giger <niklaus.giger@member.fsf.org>
2025-01-07 21:15:49 +0100

    Updated devenv for ruby 3.4

--- devenv.nix
+++ devenv.nix
@@ -3,7 +3,6 @@
 {
   # https://devenv.sh/basics/
   env.GREET = "devenv";
-  env.FREEDESKTOP_MIME_TYPES_PATH = "${pkgs.shared-mime-info}/share/mime/packages/freedesktop.org.xml";
 
   # https://devenv.sh/packages/
   packages = [ pkgs.git pkgs.libyaml ];
@@ -14,8 +13,18 @@
     ruby --version
   '';
 
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
   languages.ruby.enable = true;
-  languages.ruby.versionFile = ./.ruby-version;
+  languages.ruby.version = "3.4";
+  # Needed for gem mimemagic
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
 
   # See full reference at https://devenv.sh/reference/options/
 }
